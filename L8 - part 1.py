4752e93c330a6042de565586c47da05dd0daea94
CoreyBiluk <77625477+CoreyBiluk@users.noreply.github.com>
2021-02-22 20:45:09 -0400

    Add files via upload

---
+++ L8 - part 1.py
@@ -0,0 +1,14 @@
+# Lab 8 Part 1B
+# Corey Biluk
+# February 22, 2021
+
+from guizero import App, Slider, TextBox
+
+# Main Program    
+if __name__ == '__main__':
+    
+    app = App(title = "RC Servo ControlPOS")
+    slider1 = Slider(app, start=-90, end = 90, width=400)
+    slider2 = Slider(app, start=-90, end = 90, width=400)
+    app.display()
+
